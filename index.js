e0f10fb4e54911f108ee314d8e639d44164a6c37
Akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2020-04-03 00:23:52 +0900

    Update index.js

--- index.js
+++ index.js
@@ -24,16 +24,15 @@ stats.domElement.style.position = 'absolute';
 stats.domElement.style.top = '0px';
 document.body.appendChild(stats.domElement);
 
-let geometry=[];
-let material=[];
+let geometry = new THREE.SphereGeometry(10);
+let material = new THREE.MeshBasicMaterial( { vertexColors: THREE.FaceColors} );
+for(let l=0; l<geometry.faces.length; l++){geometry.faces[l].color.set(Math.random() * 0xCC0000);}
+
 let box=[];
 let num = 4000;
 
 for(var i=0;i<5000;i++){
-    geometry[i] = new THREE.SphereGeometry(10);
-    material[i] = new THREE.MeshBasicMaterial( { vertexColors: THREE.FaceColors} );
-    for(let l=0; l<geometry[i].faces.length; l++){geometry[i].faces[l].color.set(Math.random() * 0xCC0000);}
-    box[i] = new THREE.Mesh(geometry[i], material[i]);
+    box[i] = new THREE.Mesh(geometry, material);
     box[i].position.set((Math.random()*num)-num/2,(Math.random()*num)-num/2,(Math.random()*num)-num/2);
     scene.add(box[i]);
 }
