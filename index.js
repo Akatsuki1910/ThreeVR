cb2a856f8e8ac324d48279d8e5287cf1f84c7c0c
Akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2020-04-03 23:00:59 +0900

    a

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 var ua = [
     "iPod",
     "iPad",
@@ -12,47 +11,6 @@ function success(){
 
 function failure(){
     document.getElementById("txt").innerHTML ="このデバイスでは対応しておりません";
-=======
-console.log(1);
-
-const width = window.innerWidth;
-const height = window.innerHeight;
-
-$("body").css('height',height);
-$("body").css('width',width);
-
-const rendererThree = new THREE.WebGLRenderer({
-	canvas: document.querySelector('canvas')
-});
-rendererThree.setPixelRatio(window.devicePixelRatio);
-rendererThree.setSize(width, height);
-
-const scene = new THREE.Scene();
-
-const camera = new THREE.PerspectiveCamera(60, width / height);
-camera.position.set(0, 0, 0);
-const controls = new THREE.DeviceOrientationControls(camera, true);
-controls.connect();
-// const controls2 = new THREE.OrbitControls(camera);
-// controls2.enableDamping = true;
-
-const stats = new Stats();
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.top = '0px';
-document.body.appendChild(stats.domElement);
-
-let geometry = new THREE.SphereGeometry(10);
-let material = new THREE.MeshBasicMaterial( { vertexColors: THREE.FaceColors} );
-for(let l=0; l<geometry.faces.length; l++){geometry.faces[l].color.set(Math.random() * 0xCC0000);}
-
-let box=[];
-let num = 4000;
-
-for(var i=0;i<5000;i++){
-    box[i] = new THREE.Mesh(geometry, material);
-    box[i].position.set((Math.random()*num)-num/2,(Math.random()*num)-num/2,(Math.random()*num)-num/2);
-    scene.add(box[i]);
->>>>>>> Stashed changes
 }
 
 
