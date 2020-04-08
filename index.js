f5a94bfce9e3f1f0e678a08701f1c25bcdc0a794
Akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2020-04-09 02:03:09 +0900

    indent

--- index.js
+++ index.js
@@ -1,123 +1,147 @@
 var ua = [
-    "iPod",
-    "iPad",
-    "iPhone"
+	"iPod",
+	"iPad",
+	"iPhone"
 ];
 
-function success(){
-    document.getElementById("txt").innerHTML ="ボタンを押してください!";
-    document.getElementById("check").disabled = "";
+function success() {
+	document.getElementById("txt").innerHTML = "ボタンを押してください!";
+	document.getElementById("check").disabled = "";
 }
 
-function failure(){
-    document.getElementById("txt").innerHTML ="このデバイスでは対応しておりません";
+function failure() {
+	document.getElementById("txt").innerHTML = "このデバイスでは対応しておりません";
 }
 
 
 document.getElementById("test").innerHTML = window.navigator.userAgent;
-var iosflg=false;
-if(((window.DeviceOrientationEvent)&&('ontouchstart' in window))){
-    //mobile
+var iosflg = false;
+if (((window.DeviceOrientationEvent) && ('ontouchstart' in window))) {
+	//mobile
 	for (var i = 0; i < ua.length; i++) {
 		if (window.navigator.userAgent.indexOf(ua[i]) > 0) {
-            iosflg=true;
-            success();
-            break;
+			iosflg = true;
+			success();
+			break;
 		}
-    }
+	}
 
-    if(!iosflg && window.navigator.userAgent.indexOf("Android") > 0){
-        success();
-    }
+	if (!iosflg && window.navigator.userAgent.indexOf("Android") > 0) {
+		success();
+	}
 
 } else {
-    //pc
-    failure();
+	//pc
+	failure();
 }
 
-function check(){
-    document.getElementById("check").disabled = "disabled";
-    console.log(typeof DeviceOrientationEvent.requestPermission);
-    if(iosflg){
-        //ios
-        try{
-            DeviceOrientationEvent.requestPermission().then(res => {
-                //yes
-                if(res === 'granted'){
-                    main();
-                //no
-                }else{
-                    failure();
-                }
-            });
-        }
-        catch(e){
-            failure();
-            alert(e);
-        }
-    }else{
-        //android
-        main();
-    }
+function check() {
+	document.getElementById("check").disabled = "disabled";
+	console.log(typeof DeviceOrientationEvent.requestPermission);
+	if (iosflg) {
+		//ios
+		try {
+			DeviceOrientationEvent.requestPermission().then(res => {
+				//yes
+				if (res === 'granted') {
+					main();
+					//no
+				} else {
+					failure();
+				}
+			});
+		} catch (e) {
+			failure();
+			alert(e);
+		}
+	} else {
+		//android
+		main();
+	}
 }
 
-function main(){
-    $('#threejs').css("display","inline");
-    $('#title').css("display","none");
+function main() {
+	$('#pixiview').css("display", "inline");
+	$('#title').css("display", "none");
+	var width = window.innerWidth;
+	var height = window.innerHeight;
+	var stage = new PIXI.Container();
+	var renderer = PIXI.autoDetectRenderer({
+		width: width,
+		height: height,
+		resolution: 1,
+		antialias: true,
+		transparent: true
+	});
+	document.getElementById('pixiview').appendChild(renderer.view);
+	var word = "test";
+	var style = {
+		fontFamily: 'Arial',
+		fontSize: '40px',
+		fill: 'white',
+		fontWeight: "bold"
+	};
+	var obj = new PIXI.Text(word, style);
+	obj.position.x = width / 2;
+	obj.position.y = height / 2;
+	obj.anchor.x = 0.5;
+	obj.anchor.y = 0.5;
+	stage.addChild(obj);
 
-    const width = window.innerWidth;
-    const height = window.innerHeight;
+	const rendererThree = new THREE.WebGLRenderer({
+		canvas: document.querySelector('canvas')
+	});
+	rendererThree.setPixelRatio(window.devicePixelRatio);
+	rendererThree.setSize(width, height);
 
-    $("body").css('height',height);
-    $("body").css('width',width);
+	const scene = new THREE.Scene();
 
-    const rendererThree = new THREE.WebGLRenderer({
-        canvas: document.querySelector('canvas')
-    });
-    rendererThree.setPixelRatio(window.devicePixelRatio);
-    rendererThree.setSize(width, height);
+	const camera = new THREE.PerspectiveCamera(60, width / height);
+	camera.position.set(0, 0, 0);
+	const controls = new THREE.DeviceOrientationControls(camera, true);
+	controls.connect();
+	// const controls2 = new THREE.OrbitControls(camera);
+	// controls2.enableDamping = true;
 
-    const scene = new THREE.Scene();
+	const stats = new Stats();
+	stats.domElement.style.position = 'absolute';
+	stats.domElement.style.top = '0px';
+	document.body.appendChild(stats.domElement);
 
-    const camera = new THREE.PerspectiveCamera(60, width / height);
-    camera.position.set(0, 0, 0);
-    const controls = new THREE.DeviceOrientationControls(camera, true);
-    controls.connect();
-    // const controls2 = new THREE.OrbitControls(camera);
-    // controls2.enableDamping = true;
+	let geometry = new THREE.SphereGeometry(10);
+	let material = new THREE.MeshBasicMaterial({
+		vertexColors: THREE.FaceColors
+	});
+	for (let l = 0; l < geometry.faces.length; l++) {
+		geometry.faces[l].color.set(Math.random() * 0xCC0000);
+	}
 
-    const stats = new Stats();
-    stats.domElement.style.position = 'absolute';
-    stats.domElement.style.top = '0px';
-    document.body.appendChild(stats.domElement);
+	let box = [];
+	let num = 4000;
 
-    let geometry = new THREE.SphereGeometry(10);
-    let material = new THREE.MeshBasicMaterial( { vertexColors: THREE.FaceColors} );
-    for(let l=0; l<geometry.faces.length; l++){geometry.faces[l].color.set(Math.random() * 0xCC0000);}
+	for (var i = 0; i < 5000; i++) {
+		box[i] = new THREE.Mesh(geometry, material);
+		box[i].position.set((Math.random() * num) - num / 2, (Math.random() * num) - num / 2, (Math.random() * num) - num / 2);
+		scene.add(box[i]);
+	}
 
-    let box=[];
-    let num = 4000;
+	let g = new THREE.CylinderGeometry(5, 5, 20, 32);
+	let m = new THREE.MeshBasicMaterial({
+		color: 0xFF0000
+	});
+	let b = new THREE.Mesh(g, m);
+	scene.add(b);
+	b.position.set(0, 0, -500);
 
-    for(var i=0;i<5000;i++){
-        box[i] = new THREE.Mesh(geometry, material);
-        box[i].position.set((Math.random()*num)-num/2,(Math.random()*num)-num/2,(Math.random()*num)-num/2);
-        scene.add(box[i]);
-    }
-
-    let g = new THREE.CylinderGeometry( 5, 5, 20, 32 );
-    let m = new THREE.MeshBasicMaterial( {color: 0xFF0000} );
-    let b = new THREE.Mesh( g, m );
-    scene.add( b );
-    b.position.set(0,0,-500);
-
-    !function animate(){
-        requestAnimationFrame(animate);
-        for(var i=0;i<box.length;i++){
-            box[i].rotation.y+=0.1;
-        }
-        controls.update();
-        // controls2.update();
-        stats.update();
-        rendererThree.render(scene, camera);
-    }();
+	! function animate() {
+		requestAnimationFrame(animate);
+		for (var i = 0; i < box.length; i++) {
+			box[i].rotation.y += 0.1;
+		}
+		controls.update();
+		// controls2.update();
+		stats.update();
+		rendererThree.render(scene, camera);
+		renderer.render(stage);
+	}();
 }
